f450166630ee346153f2632c0ad2a41eea74e9ed
Mathieu PATUREL <australie.p@gmail.com>
2019-11-24 17:26:38 +1100

    cryptor: test save and load with 2 different cryptors
    this somewhat simulates saving, quiting, logging in and loading

--- cryptor_test.go
+++ cryptor_test.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"bytes"
+	"log"
 	"os"
 	"testing"
 )
 
 // FIXME: use virtual file system
-// FIXME: use some kind of pre-test/post-test to setup/teardown the folder
-func TestCryptor(t *testing.T) {
+func TestMain(m *testing.M) {
+	if err := os.MkdirAll("test-store", os.ModePerm); err != nil {
+		log.Fatalf("makdir test-store: %s", err)
+	}
+	code := m.Run()
+	if err := os.RemoveAll("test-store"); err != nil {
+		log.Fatalf("remove test-store: %s", err)
+	}
+	os.Exit(code)
+}
+
+func TestBasicCryptor(t *testing.T) {
 	password := []byte("hello world")
 
 	cryptor, err := NewCryptor(password)
 	if err != nil {
-		t.Errorf("creating cryptor: %s", err)
-	}
-
-	if err := os.MkdirAll("test-store", os.ModePerm); err != nil {
-		t.Fatalf("makdir test-store: %s", err)
+		t.Fatalf("creating cryptor: %s", err)
 	}
 
 	input := []byte("aaaaa bbbb cccc dddd")
@@ -32,10 +39,36 @@ func TestCryptor(t *testing.T) {
 	}
 
 	if !bytes.Equal(input, output) {
-		t.Errorf("Save(n); m = Load(); n should equal m\n%q\n%q", input, output)
+		t.Errorf("input should equal output\n%q\n%q", input, output)
+	}
+}
+
+// Test saving with one cryptor and loading with an other
+func TestDifferentCryptor(t *testing.T) {
+	password := []byte("some other password")
+
+	cryptorw, err := NewCryptor(password)
+	if err != nil {
+		t.Fatalf("creating writting cryptor: %s", err)
 	}
 
-	if err := os.RemoveAll("test-store"); err != nil {
-		t.Fatalf("remove test-store: %s", err)
+	input := []byte("asdf poijwqefad asdf owqiejfasldfkw")
+
+	if err := cryptorw.Save("test-store/test2", input); err != nil {
+		t.Fatalf("saving to test-store/test2: %s", err)
+	}
+
+	cryptorr, err := NewCryptor(password)
+	if err != nil {
+		t.Fatalf("create reading cryptor: %s", err)
+	}
+
+	output, err := cryptorr.Load("test-store/test2")
+	if err != nil {
+		t.Fatalf("loading from test-store/test2: %s", err)
+	}
+
+	if !bytes.Equal(input, output) {
+		t.Errorf("input should equal output\n%q\n%q", input, output)
 	}
 }
