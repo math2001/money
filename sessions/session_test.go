1ca6b944ed63246ee2bf6f24655bf6bb167ac5ae
Mathieu PATUREL <australie.p@gmail.com>
2019-12-12 10:50:52 +1100

    sessions: add session manager

---
+++ sessions/session_test.go
@@ -0,0 +1,127 @@
+package sessions_test
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/math2001/money/sessions"
+)
+
+const cookieName = "session"
+
+func TestNoSession(t *testing.T) {
+	s := NewS(t)
+	req := httptest.NewRequest(http.MethodGet, "/api/sample", nil)
+	obj, err := s.Load(req)
+	if err != nil {
+		t.Fatalf("loading from empty request: %s", err)
+	}
+	if obj != nil {
+		t.Fatalf("loading from empty request, got non-nil obj: %s", obj)
+	}
+}
+
+func TestEmpty(t *testing.T) {
+	s := NewS(t)
+
+	req := httptest.NewRequest(http.MethodGet, "/api/sample", nil)
+	req.AddCookie(&http.Cookie{
+		Name:  cookieName,
+		Value: "",
+	})
+
+	obj, err := s.Load(req)
+	if err == nil {
+		t.Fatalf("loading empty cookie, should have error")
+	}
+	if obj != nil {
+		t.Fatalf("loading from empty cookie, got non-nil obj: %s", obj)
+	}
+}
+
+func TestNormal(t *testing.T) {
+	s := NewS(t)
+	expected := map[string]interface{}{
+		"hello": "world",
+	}
+
+	w := httptest.NewRecorder()
+	s.Save(w, expected)
+
+	req := httptest.NewRequest(http.MethodGet, "/api/sample", nil)
+	req.AddCookie(w.Result().Cookies()[0])
+
+	actual, err := s.Load(req)
+	if err != nil {
+		t.Fatalf("loading session: %s", err)
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("original != loaded: \n%v\n%v", actual, expected)
+	}
+}
+
+func TestHack(t *testing.T) {
+	s := NewS(t)
+
+	w := httptest.NewRecorder()
+	s.Save(w, map[string]interface{}{
+		"id": 2,
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/api/sample", nil)
+	req.AddCookie(&http.Cookie{
+		Name:  cookieName,
+		Value: craftSession(t),
+	})
+
+	_, err := s.Load(req)
+	if err == nil {
+		t.Fatalf("hacked session! should have error")
+	}
+
+	// horrible error checking, I will test the warning system that will
+	// be implemented later on
+	if !strings.Contains(err.Error(), "signature") {
+		t.Fatalf("err should have been about signature not matching, got %s", err)
+	}
+
+}
+
+func NewS(t *testing.T) *sessions.S {
+	t.Helper()
+	key := make([]byte, 32)
+	seed := time.Now().UnixNano()
+	t.Logf("seed: %d\n", seed)
+	if _, err := io.ReadFull(rand.New(rand.NewSource(seed)), key); err != nil {
+		t.Fatalf("generating deterministic key: %s", err)
+	}
+	s, err := sessions.NewS(&sessions.Config{
+		Key:        key,
+		CookieName: cookieName,
+	})
+	if err != nil {
+		t.Fatalf("creating S: %s", err)
+	}
+	return s
+}
+
+func craftSession(t *testing.T) string {
+	t.Helper()
+
+	a := base64.StdEncoding.EncodeToString([]byte("sha256"))
+	b := base64.StdEncoding.EncodeToString([]byte(`{"id":3}`))
+	h := hmac.New(sha256.New, []byte("random key..."))
+	h.Write([]byte(fmt.Sprintf("%s.%s", a, b)))
+	c := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	return fmt.Sprintf("%s.%s.%s", a, b, c)
+}
---
+++ sessions/sessions.go
@@ -0,0 +1,162 @@
+package sessions
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+// sessions provide a way to store user information on their end (in a cookie)
+
+// cookie session format. Each part is base64 encoded
+// alg.payload.signature
+// it's heavily inspired by JWT
+
+// S should be instantiated once, and then used for every
+// request
+type S struct {
+	cookieName string
+	// only one support signature algorithm, sha256
+	alg string
+
+	// key is the program's secret that's used to sign the cookies payload
+	// IF THIS KEY IS LEAKED, ANYBODY COULD CLAIM TO BE ANYBODY ELSE EXTREMELY
+	// EASILY
+	key []byte
+}
+
+func NewS(config *Config) (*S, error) {
+	cn := config.CookieName
+	if cn == "" {
+		cn = "session"
+	}
+	alg := config.Algorithm
+	if alg == "" {
+		alg = "sha256"
+	}
+
+	if alg != "sha256" {
+		return nil, fmt.Errorf("unsuported algorithm: %q", alg)
+	}
+
+	if len(config.Key) == 0 {
+		return nil, fmt.Errorf("empty signature key")
+	}
+
+	return &S{
+		cookieName: cn,
+		alg:        alg,
+		key:        config.Key,
+	}, nil
+}
+
+type Config struct {
+	CookieName string
+	Algorithm  string
+	Key        []byte
+}
+
+type secret []byte
+
+func (secret) String() string {
+	return "[secret]"
+}
+
+func (s *S) Save(w http.ResponseWriter, obj map[string]interface{}) error {
+	payload, err := json.Marshal(obj)
+	if err != nil {
+		return fmt.Errorf("marshaling obj: %s", err)
+	}
+
+	var token bytes.Buffer
+
+	encoder := base64.NewEncoder(base64.StdEncoding, &token)
+	if _, err := encoder.Write([]byte(s.alg)); err != nil {
+		return fmt.Errorf("alg: %s", err)
+	}
+
+	if err := encoder.Close(); err != nil {
+		return fmt.Errorf("closing (flushing) partial: %s", err)
+	}
+
+	token.Write([]byte("."))
+	if _, err := encoder.Write(payload); err != nil {
+		return fmt.Errorf("payload: %s", err)
+	}
+
+	if err := encoder.Close(); err != nil {
+		return fmt.Errorf("closing (flushing) partial: %s", err)
+	}
+	token.Write([]byte("."))
+
+	h := hmac.New(sha256.New, s.key)
+	h.Write(token.Bytes()) // sign the whole head.base64payload. (note the extra dot)
+	if _, err := encoder.Write(h.Sum(nil)); err != nil {
+		return fmt.Errorf("signature: %s", err)
+	}
+
+	if err := encoder.Close(); err != nil {
+		return fmt.Errorf("closing (flushing) partial: %s", err)
+	}
+
+	// save bytes to cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:  s.cookieName,
+		Value: token.String(),
+
+		MaxAge:   7 * 24 * 60 * 60,
+		HttpOnly: true,
+	})
+
+	return nil
+}
+
+func (s *S) Load(r *http.Request) (map[string]interface{}, error) {
+	cookie, err := r.Cookie(s.cookieName)
+	if err == http.ErrNoCookie {
+		return nil, nil
+	}
+	splits := strings.Split(cookie.Value, ".")
+	if len(splits) != 3 {
+		return nil, fmt.Errorf("session cookie: wrong splits")
+	}
+
+	alg, err := base64.StdEncoding.DecodeString(splits[0])
+	if err != nil {
+		return nil, fmt.Errorf("session coookie: reading alg: %s", err)
+	}
+
+	if string(alg) != s.alg {
+		return nil, fmt.Errorf("session cookie: unknown alg: %q", alg)
+	}
+
+	payload, err := base64.StdEncoding.DecodeString(splits[1])
+	if err != nil {
+		return nil, fmt.Errorf("session cookie: reading payload: %s", err)
+	}
+
+	signature, err := base64.StdEncoding.DecodeString(splits[2])
+	if err != nil {
+		return nil, fmt.Errorf("session cookie: reading payload: %s", err)
+	}
+
+	h := hmac.New(sha256.New, s.key)
+	h.Write([]byte(fmt.Sprintf("%s.%s.", splits[0], splits[1])))
+
+	if !hmac.Equal(h.Sum(nil), signature) {
+		log.Println("!! Warning !! potential attack on cookie signature")
+		return nil, fmt.Errorf("session cookie: payload signature didn't match")
+	}
+
+	obj := make(map[string]interface{})
+	if err := json.Unmarshal(payload, &obj); err != nil {
+		return nil, fmt.Errorf("session cookie: unmarshaling: %s", err)
+	}
+	return obj, nil
+}
