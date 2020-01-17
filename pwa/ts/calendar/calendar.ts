07b008b2331465be0a264362490b1a4ef873a832
Mathieu PATUREL <australie.p@gmail.com>
2020-01-17 16:07:26 +1100

    calendar: add basic numbers

--- pwa/ts/calendar/calendar.ts
+++ pwa/ts/calendar/calendar.ts
@@ -12,21 +12,37 @@ export default class Calendar {
   }
 
   generateHTML() {
-    const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
     const today = new Date()
     this.monthname.textContent = months[today.getMonth()]
 
+    let firstWeekdayIndex = today.getDay() - (today.getDate() % 7) + 1
+    console.log(firstWeekdayIndex, "should be wed", 3)
+
+    const row = document.createElement('tr')
+    for (let weekday of weekdays) {
+      const th = document.createElement('th')
+      th.textContent = weekday
+      row.appendChild(th)
+    }
+    this.month.appendChild(row)
 
     for (let i = 0; i < 5; i++) {
       const row = document.createElement("tr")
       for (let j = 0; j < 7; j++) {
         const day = document.createElement('td')
-        day.innerHTML = weekdays[j] + ' ' + 
+        day.innerHTML = '' + (i * 7 + j - firstWeekdayIndex + 1)
         row.appendChild(day)
       }
+      this.month.appendChild(row)
     }
+  }
+
+  getDayOfMonth(weekIndex, dayIndex) {
+    const today = new Date()
+    today
   }
 
   setup() {
