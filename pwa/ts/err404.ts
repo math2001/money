cdb8d8cfd5ba3e0fa9551ed32e6d2b9559d7535a
Mathieu PATUREL <australie.p@gmail.com>
2020-01-18 17:33:35 +1100

    pwa: remove pwa to make room for react

--- pwa/ts/err404.ts
+++
@@ -1,8 +0,0 @@
-export default class Err404 {
-  section: HTMLElement;
-  constructor(section: HTMLElement) {
-    this.section = section
-  }
-  setup() {}
-  teardown() {}
-}
--- pwa/ts/payments/index.ts
+++
@@ -1,9 +0,0 @@
-import AddManual from "./add-manual.js";
-import List from "./list.js";
-import Camera from "./camera.js";
-
-export default {
-  addManual: AddManual,
-  list: List,
-  camera: Camera,
-};
