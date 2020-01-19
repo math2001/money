1ec51f6d94ebc0be6f6fb801e052dbd050b54a63
Mathieu PATUREL <australie.p@gmail.com>
2020-01-19 19:01:21 +1100

    pwa/calendar: support moving by month

--- pwa/src/calendar/Calendar.tsx
+++ pwa/src/calendar/Calendar.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Month from "./Month";
-import Header from "./Header";
+import Header, { MoveType } from "./Header";
 import { months, days } from "./data";
 import "./Calendar.css";
 
 interface Props {}
 
 interface State {
-  month: number;
   year: number;
+  month: number;
   // dayOfMonth is the number of the day in the month (1st, 2nd, ... 31st)
   // just like Date::setDate
   dayOfMonth: number;
@@ -24,6 +24,23 @@ class Calendar extends React.Component<Props, State> {
       month: today.getMonth(),
       dayOfMonth: today.getDate(),
     };
+
+    this.move = this.move.bind(this);
+  }
+
+  move(type: MoveType, amount: number) {
+    this.setState(state => {
+      const newDate = new Date(
+        state.year + (type === MoveType.Year ? amount : 0),
+        state.month + (type === MoveType.Month ? amount : 0),
+        state.dayOfMonth,
+      );
+      return {
+        year: newDate.getFullYear(),
+        month: newDate.getMonth(),
+        dayOfMonth: newDate.getDate(),
+      };
+    });
   }
 
   render() {
@@ -33,7 +50,11 @@ class Calendar extends React.Component<Props, State> {
     }
     return (
       <section className="calendar">
-        <Header year={this.state.year} month={this.state.month} />
+        <Header
+          year={this.state.year}
+          month={this.state.month}
+          move={this.move}
+        />
 
         <Month
           year={this.state.year}
--- pwa/src/calendar/Header.tsx
+++ pwa/src/calendar/Header.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { months } from "./data";
 
-const Header: React.FC<{ month: number; year: number }> = ({ month, year }) => {
+const Header: React.FC<{
+  month: number;
+  year: number;
+  move: (movetype: MoveType, amount: number) => void;
+}> = ({ month, year, move }) => {
   return (
     <header className="calendar-header">
+      <button onClick={() => move(MoveType.Month, -1)}>Previous Month</button>
       {months[month]} {year}
+      <button onClick={() => move(MoveType.Month, 1)}>Next Month</button>
     </header>
   );
 };
 
 export default Header;
+
+export enum MoveType {
+  Month,
+  Year,
+}
